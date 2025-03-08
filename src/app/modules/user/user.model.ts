282d5547922e18e435e147a172ca5c639416091b
meskatabusayed <meskatabusayed@gmail.com>
2025-03-09 04:35:38 +0530

    refector code

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,24 +1,25 @@
-import { Schema, model } from "mongoose";
-import { TUser } from "./user.interface";
+import { Schema, model } from 'mongoose';
+import { TUser } from './user.interface';
 
 const userSchema = new Schema<TUser>(
   {
     id: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    needsPasswordChange: { type: Boolean,  default: true },
+    needsPasswordChange: { type: Boolean, default: true },
     role: {
       type: String,
-      enum: ["admin", "Student", "faculty"],
+      enum: ['admin', 'Student', 'faculty'],
       required: true,
     },
     status: {
       type: String,
-      enum: ["in-progress", "blocked"],
+      enum: ['in-progress', 'blocked'],
+      default : "in-progress",
       required: true,
     },
-    isDeleted: { type: Boolean, required: true, default: false },
+    isDeleted: { type: Boolean,  default: false },
   },
   { timestamps: true } // Enables createdAt & updatedAt fields
 );
 
-export const UserModel = model<TUser>("User", userSchema);
+export const UserModel = model<TUser>('User', userSchema);
--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,19 +1,14 @@
-import { z } from "zod";
+import { z } from 'zod';
 
- const userValidationSchema = z.object({
-  id: z.string().min(1, "ID is required"),
-  password: z.string().min(6, "Password must be at least 6 characters long"),
-  needsPasswordChange: z.boolean().default(true).optional(),
-  role: z.enum(["admin", "Student", "faculty"], {
-    errorMap: () => ({ message: "Role must be either admin, Student, or faculty" }),
-  }),
-  status: z.enum(["in-progress", "blocked"], {
-    errorMap: () => ({ message: "Status must be either in-progress or blocked" }),
-  }),
-  isDeleted: z.boolean().default(false),
+const userValidationSchema = z.object({
+  password: z
+    .string({
+      invalid_type_error: 'Password must be a string',
+    })
+    .min(6, 'Password must be at least 6 characters long')
+    .optional()
 });
 
 export const userValidation = {
-    userValidationSchema,
-
-}
+  userValidationSchema,
+};
