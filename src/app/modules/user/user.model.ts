c1550a8e058c2b799f5f91a33a4a399a43a05807
meskatabusayed <meskatabusayed@gmail.com>
2025-03-09 02:59:05 +0530

    user model done

---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,24 @@
+import { Schema, model } from "mongoose";
+import { TUser } from "./user.interface";
+
+const userSchema = new Schema<TUser>(
+  {
+    id: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    needsPasswordChange: { type: Boolean, required: true, default: true },
+    role: {
+      type: String,
+      enum: ["admin", "Student", "faculty"],
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["in-progress", "blocked"],
+      required: true,
+    },
+    isDeleted: { type: Boolean, required: true, default: false },
+  },
+  { timestamps: true } // Enables createdAt & updatedAt fields
+);
+
+export const UserModel = model<TUser>("User", userSchema);
