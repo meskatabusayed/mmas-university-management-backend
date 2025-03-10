e745c2e3d1a3166f23d2192e793def7e362d49e6
meskatabusayed <meskatabusayed@gmail.com>
2025-03-11 04:56:40 +0530

    user utils file create and generated Student id done

---
+++ src/app/modules/user/user.utils.ts
@@ -0,0 +1,12 @@
+import { TAcademicSemester } from "../academicSemester/academicSemester.interface";
+
+
+export const genereatedStudentId = async(payload : TAcademicSemester) => {
+    //first time 0000
+    const currentId = (0).toString().padStart(4 , '0');
+    let incrementId = (Number(currentId)+1).toString();
+    incrementId = `${payload.year}${payload.code}${incrementId}`;
+    return incrementId;
+
+
+}
