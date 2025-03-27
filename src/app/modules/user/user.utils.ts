b2c2c595f96c1d5fef2e4b00dd4e1fb444bc732d
meskatabusayed <meskatabusayed@gmail.com>
2025-03-27 20:29:24 +0530

    academic faculty interface create done

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -1,23 +1,40 @@
-import { TAcademicSemester } from "../academicSemester/academicSemester.interface";
-import { User } from "./user.model";
+import { TAcademicSemester } from '../academicSemester/academicSemester.interface';
+import { User } from './user.model';
 
-const findLastStudentId = async() => {
-    const lastStudent = await User.findOne({
-        role : 'Student'
-    } , {
-        id : 1,
-    })
-    .sort({createdAt : -1})
+const findLastStudentId = async () => {
+  const lastStudent = await User.findOne(
+    {
+      role: 'Student',
+    },
+    {
+      id: 1,
+    }
+  )
+    .sort({ createdAt: -1 })
     .lean();
-    return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
-}
+  return lastStudent?.id ? lastStudent.id : undefined;
+};
 
-export const genereatedStudentId = async(payload : TAcademicSemester) => {
-    //first time 0000
-    const currentId = await findLastStudentId() || (0).toString();
-    let incrementId = (Number(currentId)+1).toString().padStart(4 , '0');
-    incrementId = `${payload.year}${payload.code}${incrementId}`;
-    return incrementId;
+export const genereatedStudentId = async (payload: TAcademicSemester) => {
+  //first time 0000
+  let currentId = (0).toString();
+  const lastStudentId = await findLastStudentId();
+  //id : -- > 2030 01 0001
 
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6);
+  const lastStudentYear = lastStudentId?.substring(0, 4);
+  const currentSemesterCode = String(payload.code);
+  const currentyear = String(payload.year);
 
-}
+  if (
+    lastStudentId &&
+    lastStudentSemesterCode === currentSemesterCode &&
+    lastStudentYear === currentyear
+  ) {
+    currentId = lastStudentId.substring(6); //0001
+  }
+
+  let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
+  incrementId = `${payload.year}${payload.code}${incrementId}`;
+  return incrementId;
+};
