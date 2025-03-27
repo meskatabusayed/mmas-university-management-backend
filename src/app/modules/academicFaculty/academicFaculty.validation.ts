75070e29124e4ef19db0deb2e869643633b125c7
meskatabusayed <meskatabusayed@gmail.com>
2025-03-27 21:00:27 +0530

    academic faculty validation create

---
+++ src/app/modules/academicFaculty/academicFaculty.validation.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+ const createAcademicFacultyValidationSchema = z.object({
+  name: z.string().min(1, "Name is required").max(100, "Name is too long"),
+});
+
+
+export const academicFacultyValidationSchema = {
+    createAcademicFacultyValidationSchema
+}
